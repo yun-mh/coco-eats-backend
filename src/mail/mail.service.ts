d6be30839aa9bf9f623f945cc4dd7f1b16c40f74
Minho Yun <minhoyun.dev@gmail.com>
2021-04-17 22:02:55 +0900

    Test sendMail with Jest and change the test target to only service.ts

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -10,11 +10,11 @@ export class MailService {
     @Inject(CONFIG_OPTIONS) private readonly options: MailModuleOptions,
   ) {}
 
-  private async sendEmail(
+  async sendEmail(
     subject: string,
     template: string,
     emailVariables: EmailVariable[],
-  ) {
+  ): Promise<boolean> {
     const form = new FormData();
     form.append('from', `Coco Eats <mailgun@${this.options.domain}>`);
     form.append('to', 'canivas@naver.com'); // mailgun無料利用のため、認証済みのアドレス宛にしか送信できない。
@@ -23,17 +23,20 @@ export class MailService {
     emailVariables.forEach((eVar) => form.append(`v:${eVar.key}`, eVar.value));
 
     try {
-      await got(`https://api.mailgun.net/v3/${this.options.domain}/messages`, {
-        method: 'POST',
-        headers: {
-          Authorization: `Basic ${Buffer.from(
-            `api:${this.options.apiKey}`,
-          ).toString('base64')}`,
+      await got.post(
+        `https://api.mailgun.net/v3/${this.options.domain}/messages`,
+        {
+          headers: {
+            Authorization: `Basic ${Buffer.from(
+              `api:${this.options.apiKey}`,
+            ).toString('base64')}`,
+          },
+          body: form,
         },
-        body: form,
-      });
+      );
+      return true;
     } catch (e) {
-      console.log(e);
+      return false;
     }
   }
 
