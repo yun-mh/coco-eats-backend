506cbf4025507ec115221c2af88f60501031bc18
Minho Yun <minhoyun.dev@gmail.com>
2021-04-27 17:11:08 +0900

    Test e2e of createAccount

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -4,6 +4,14 @@ import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
 import { getConnection } from 'typeorm';
 
+jest.mock('got', () => {
+  return {
+    post: jest.fn(),
+  };
+});
+
+const GRAPHQL_ENDPOINT = '/graphql';
+
 describe('UserModule (e2e)', () => {
   let app: INestApplication;
 
@@ -21,10 +29,56 @@ describe('UserModule (e2e)', () => {
     app.close();
   });
 
-  it.todo('me');
+  describe('createAccount', () => {
+    const EMAIL = 'minho8912@test.com';
+
+    it('should create account', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `mutation {
+          createAccount(input: {
+            email: "${EMAIL}",
+            password: "12345",
+            role: Owner
+          }) {
+            ok
+            error
+          }
+        }`,
+        })
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.data.createAccount.ok).toBe(true);
+          expect(res.body.data.createAccount.error).toBe(null);
+        });
+    });
+
+    it('should fail if account have already existed', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `mutation {
+          createAccount(input: {
+            email: "${EMAIL}",
+            password: "12345",
+            role: Owner
+          }) {
+            ok
+            error
+          }
+        }`,
+        })
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.data.createAccount.ok).toBe(false);
+          expect(res.body.data.createAccount.error).toEqual(expect.any(String));
+        });
+    });
+  });
   it.todo('userProfile');
-  it.todo('createAccount');
   it.todo('login');
-  it.todo('editProfile');
+  it.todo('me');
   it.todo('verifyEmail');
+  it.todo('editProfile');
 });
